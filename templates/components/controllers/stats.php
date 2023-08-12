e951398f0037b6b7bec8e13f3e289815e3da4ff7
Bartosz Szewczyk <bszewczyk@intype.pl>
2023-08-12 10:59:41 +0200

    WPML Localization static strings

--- templates/components/controllers/stats.php
+++ templates/components/controllers/stats.php
@@ -4,7 +4,7 @@ class StatsComponent{
   function __construct(){
     $context = Timber::context();
     $img_path = '/wp-content/themes/dziubdziub/img/';
-    $context['title_emblemat_src'] = $img_path."components/stats/stats_title-eagle.svg";
+    $context['title_emblem_src'] = $img_path."components/stats/stats_title-eagle.svg";
     Timber::render('templates/components/views/stats.twig', $context);
   }
 }

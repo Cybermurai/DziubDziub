27b95115114cb44d17982ac5228ee2280bd01f25
Bartosz Szewczyk <bszewczyk@intype.pl>
2023-08-12 10:56:02 +0200

    WPML Localization static strings

--- templates/components/controllers/hero.php
+++ templates/components/controllers/hero.php
@@ -8,7 +8,7 @@ class HeroComponent{
     $img_path = '/wp-content/themes/dziubdziub/img/';
 
     $context['eagle_src'] = $img_path."components/hero/hero_eagle.png";
-    $context['eagle_emblemat_src'] = $img_path."components/hero/hero_title-eagle.svg";
+    $context['eagle_emblem_src'] = $img_path."components/hero/hero_title-eagle.svg";
     $context['socials'] = [
       'fb' => $img_path.'icons/fb.svg',
       'instagram' => $img_path.'icons/instagram.svg'
